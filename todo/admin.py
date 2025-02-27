fdb50dcec023d6d1660d1ba2f3130ccff3cf12ac
vibsin9322 <vibsin74@gmail.com>
2025-02-27 14:40:52 +0900

    todo 모델수정
    description blank true추가

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import todo
 # Register your models here.
+admin.site.register(todo)
+
+class TodoAdmin(admin.ModelAdmin):
+    pass
--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class todo(models.Model):
     name = models.CharField(max_length=100)
-    description =  models.TextField()
+    description =  models.TextField(blank=True)
     complete = models.BooleanField(default =False)
     exp = models.PositiveIntegerField(default=0)
     complete_at = models.DateTimeField(null=True, blank=True)
